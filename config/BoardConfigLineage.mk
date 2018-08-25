2e8864d60594448f800b4f660f8fbfd2b73e63e7
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    lineage: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -1,2 +1,6 @@
 include vendor/proton/config/BoardConfigKernel.mk
 include vendor/proton/config/BoardConfigSoong.mk
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/proton/config/BoardConfigQcom.mk
+endif
