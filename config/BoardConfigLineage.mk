7a6350ba02d8d7436d47f678dc15baff1d47d97b
YumeMichi <do4suki@gmail.com>
2021-11-06 01:17:23 +0800

    products: Switch to GMS from Project-Awaken
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I352e12fd693f29784a426eb43b0d8e47402bc189

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -4,3 +4,7 @@ include vendor/proton/config/BoardConfigSoong.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/proton/config/BoardConfigQcom.mk
 endif
+
+ifeq ($(WITH_GMS),true)
+include vendor/gms/products/board.mk
+endif
